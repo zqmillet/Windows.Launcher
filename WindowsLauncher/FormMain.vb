4ac7df5a4615b9b469bac7a5c2aab775412a4b64
zqmillet <zqmillet@qq.com>
2015-10-04 17:43:49 +0800

    Finish the interface

--- WindowsLauncher/FormMain.vb
+++ WindowsLauncher/FormMain.vb
@@ -2,21 +2,75 @@
     Private Const FormWidth = 300
 
     Private TableLayoutPanel As New TableLayoutPanel
+    Private LabelName As New Label
+    Private TextBoxName As New TextBox
     Private LabelFullPath As New Label
     Private TextBoxFullPath As New TextBox
     Private LabelParameter As New Label
     Private TextBoxParameter As New TextBox
     Private ButtonBrowser As New Button
+    Private LabelCondition As New Label
+    Private TextBoxCondition As New TextBox
+
+    Private ButtonOption As New Button
+    Private ButtonAdd As New Button
+
+    Private ListView As New ListView
 
     Public Sub New()
 
         InitializeComponent()
-
         InitialInterface()
 
     End Sub
 
     Private Sub InitialInterface()
+        With TableLayoutPanel
+            '.BorderStyle = BorderStyle.FixedSingle
+            '.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single
+
+            .ColumnStyles.Add(New ColumnStyle(SizeType.Absolute, 35.0F))
+            .ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 60.0F))
+            .ColumnStyles.Add(New ColumnStyle(SizeType.Absolute, 40.0F))
+            .ColumnStyles.Add(New ColumnStyle(SizeType.Absolute, 40.0F))
+
+            .ColumnCount = 4
+            .RowCount = 6
+
+            .Dock = DockStyle.Fill
+            .Padding = New Padding(2)
+
+
+            .Controls.Add(LabelName, 0, 0)
+            .SetColumnSpan(TextBoxName, 3)
+            .Controls.Add(TextBoxName, 1, 0)
+
+            .Controls.Add(LabelFullPath, 0, 1)
+            .SetColumnSpan(TextBoxFullPath, 2)
+            .Controls.Add(TextBoxFullPath, 1, 1)
+            .Controls.Add(ButtonBrowser, 3, 1)
+
+            .Controls.Add(LabelParameter, 0, 2)
+            .SetColumnSpan(TextBoxParameter, 3)
+            .Controls.Add(TextBoxParameter, 1, 2)
+
+            .Controls.Add(LabelCondition, 0, 3)
+            .SetColumnSpan(TextBoxCondition, 3)
+            .Controls.Add(TextBoxCondition, 1, 3)
+
+            .Controls.Add(ButtonOption, 2, 4)
+            .Controls.Add(ButtonAdd, 3, 4)
+
+            .SetColumnSpan(ListView, 4)
+            .Controls.Add(ListView, 0, 5)
+        End With
+
+        With LabelName
+            .Text = "名称"
+            .Dock = DockStyle.Fill
+            .TextAlign = ContentAlignment.MiddleLeft
+        End With
+
         With LabelFullPath
             .Text = "路径"
             .Dock = DockStyle.Fill
@@ -29,43 +83,75 @@
             .TextAlign = ContentAlignment.MiddleLeft
         End With
 
+        With LabelCondition
+            .Text = "条件"
+            .Dock = DockStyle.Fill
+            .TextAlign = ContentAlignment.MiddleLeft
+        End With
+
+        With TextBoxName
+            .Margin = New Padding(1)
+            .Dock = DockStyle.Fill
+        End With
+
+        With TextBoxFullPath
+            .Margin = New Padding(1)
+            .Dock = DockStyle.Fill
+        End With
+
+        With TextBoxParameter
+            .Margin = New Padding(1)
+            .Dock = DockStyle.Fill
+        End With
+
+        With TextBoxCondition
+            .Margin = New Padding(1)
+            .Dock = DockStyle.Fill
+        End With
+
         With ButtonBrowser
             .Text = "浏览"
+            .Margin = New Padding(0)
             .Dock = DockStyle.Fill
         End With
 
-        With TableLayoutPanel
-            '.BorderStyle = BorderStyle.FixedSingle
-            .CellBorderStyle = TableLayoutPanelCellBorderStyle.Single
-            .ColumnCount = 3
-            .RowCount = 5
+        With ButtonAdd
+            .Text = "添加"
+            .Margin = New Padding(0)
             .Dock = DockStyle.Fill
-
-            .ColumnStyles.Add(New ColumnStyle(SizeType.Absolute, 40))
-            .ColumnStyles.Add(New ColumnStyle(SizeType.AutoSize))
-            .ColumnStyles.Add(New ColumnStyle(SizeType.Absolute, 40))
-
-            'For i As Integer = 0 To .RowCount - 1
-            '    .RowStyles.Add(New RowStyle(ContentAlignment))
-            'Next
-
-            .Controls.Add(LabelFullPath, 0, 0)
-            .Controls.Add(TextBoxFullPath, 1, 0)
-            .Controls.Add(ButtonBrowser, 2, 0)
-
-            .Controls.Add(LabelParameter, 0, 1)
-            .Controls.Add(TextBoxParameter, 1, 1)
-            ' There is a problem in this line
-            .SetColumnSpan(TextBoxParameter, 2)
         End With
+
+        With ButtonOption
+            .Text = "设置"
+            .Margin = New Padding(0)
+            .Dock = DockStyle.Fill
+        End With
+
+        InitialListView()
 
         With Me
             .Text = "Windows Launcher"
             .Size = New Size(FormWidth, .Height)
             .MaximizeBox = False
-            .FormBorderStyle = FormBorderStyle.FixedSingle
+            .MinimumSize = .Size
+            ' .FormBorderStyle = FormBorderStyle.FixedSingle
             .Icon = New Icon(Application.StartupPath & "\icon\MainIcon.ico")
             .Controls.Add(TableLayoutPanel)
+        End With
+    End Sub
+
+    Private Sub InitialListView()
+        With ListView
+            .Margin = New Padding(1)
+            .Dock = DockStyle.Fill
+            .View = View.Details
+            .GridLines = True
+
+            .Columns.Add("序号")
+            .Columns.Add("名称")
+            .Columns.Add("路径")
+            .Columns.Add("参数")
+            .Columns.Add("条件")
         End With
     End Sub
 
